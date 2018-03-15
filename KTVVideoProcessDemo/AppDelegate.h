a71e7f7847e401899bb0f7bfefdf490d01bac605
Single <libobjc@gmail.com>
2018-03-15 11:00:27 +0800

    Initial Commit

---
+++ KTVVideoProcessDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KTVVideoProcessDemo
+//
+//  Created by Single on 2018/3/15.
+//  Copyright © 2018年 Single. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KTVVideoProcessDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KTVVideoProcessDemo
+//
+//  Created by Single on 2018/3/15.
+//  Copyright © 2018年 Single. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
