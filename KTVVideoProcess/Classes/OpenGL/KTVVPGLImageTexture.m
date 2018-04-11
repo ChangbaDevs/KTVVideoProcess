1ee363f45f0e0bffcb5af4e4657ca33b7b17b066
Single <libobjc@gmail.com>
2018-04-11 19:11:06 +0800

    update image texture

--- KTVVideoProcess/Classes/OpenGL/KTVVPGLImageTexture.m
+++ KTVVideoProcess/Classes/OpenGL/KTVVPGLImageTexture.m
@@ -118,22 +118,14 @@
         dataFromImageDataProvider = CGDataProviderCopyData(CGImageGetDataProvider(cgImage));
         imageData = (GLubyte *)CFDataGetBytePtr(dataFromImageDataProvider);
     }
-    
+    glActiveTexture(GL_TEXTURE3);
     glGenTextures(1, &_texture);
     glBindTexture(GL_TEXTURE_2D, _texture);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexImage2D(GL_TEXTURE_2D,
-                 0,
-                 GL_RGBA,
-                 _size.width,
-                 _size.height,
-                 0,
-                 format,
-                 GL_UNSIGNED_BYTE,
-                 imageData);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _size.width, _size.height, 0, format, GL_UNSIGNED_BYTE, imageData);
     glBindTexture(GL_TEXTURE_2D, 0);
     
     if (shouldRedrawUsingCoreGraphics)
