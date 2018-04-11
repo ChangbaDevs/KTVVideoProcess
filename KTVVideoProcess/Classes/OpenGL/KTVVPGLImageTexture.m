8e20858064074b01db89cb2f789cde2c3cd36a41
Single <libobjc@gmail.com>
2018-04-11 17:19:36 +0800

    update image texture

--- KTVVideoProcess/Classes/OpenGL/KTVVPGLImageTexture.m
+++ KTVVideoProcess/Classes/OpenGL/KTVVPGLImageTexture.m
@@ -10,9 +10,7 @@
 
 @interface KTVVPGLImageTexture ()
 
-@property (nonatomic, copy) NSString * path;
 @property (nonatomic, strong) UIImage * image;
-@property (nonatomic, strong) GLKTextureInfo * textureInfo;
 
 @end
 
@@ -22,7 +20,8 @@
 {
     if (self = [super init])
     {
-        _path = path;
+        _image = [UIImage imageWithContentsOfFile:path];
+        _size = KTVVPSizeMake(_image.size.width, _image.size.height);
     }
     return self;
 }
@@ -32,39 +31,126 @@
     if (self = [super init])
     {
         _image = image;
+        _size = KTVVPSizeMake(_image.size.width, _image.size.height);
     }
     return self;
 }
 
 - (void)dealloc
 {
-    NSAssert(_texture == 0 && _textureInfo == nil, @"must call destory befor dealloc.");
+    NSAssert(_texture == 0, @"must call destory befor dealloc.");
 }
 
 - (void)uploadIfNeeded
 {
-    if (_texture && _textureInfo)
+    if (_texture)
     {
         return;
     }
-    if (_image)
+    
+    CGImageRef cgImage = _image.CGImage;
+    GLubyte * imageData = NULL;
+    CFDataRef dataFromImageDataProvider = NULL;
+    GLenum format = GL_BGRA;
+    BOOL shouldRedrawUsingCoreGraphics = NO;
+    if (CGImageGetBytesPerRow(cgImage) != _size.width * 4
+        || CGImageGetBitsPerPixel(cgImage) != 32
+        || CGImageGetBitsPerComponent(cgImage) != 8)
     {
-        _textureInfo = [GLKTextureLoader textureWithCGImage:_image.CGImage options:nil error:nil];
+        shouldRedrawUsingCoreGraphics = YES;
     }
-    else if (_path)
+    else
     {
-        _textureInfo = [GLKTextureLoader textureWithContentsOfFile:_path options:nil error:nil];
+        CGBitmapInfo bitmapInfo = CGImageGetBitmapInfo(cgImage);
+        if ((bitmapInfo & kCGBitmapFloatComponents) != 0)
+        {
+            shouldRedrawUsingCoreGraphics = YES;
+        }
+        else
+        {
+            CGBitmapInfo byteOrderInfo = bitmapInfo & kCGBitmapByteOrderMask;
+            if (byteOrderInfo == kCGBitmapByteOrder32Little)
+            {
+                CGImageAlphaInfo alphaInfo = bitmapInfo & kCGBitmapAlphaInfoMask;
+                if (alphaInfo != kCGImageAlphaPremultipliedFirst
+                    && alphaInfo != kCGImageAlphaFirst
+                    && alphaInfo != kCGImageAlphaNoneSkipFirst)
+                {
+                    shouldRedrawUsingCoreGraphics = YES;
+                }
+            }
+            else if (byteOrderInfo == kCGBitmapByteOrderDefault
+                     || byteOrderInfo == kCGBitmapByteOrder32Big)
+            {
+                CGImageAlphaInfo alphaInfo = bitmapInfo & kCGBitmapAlphaInfoMask;
+                if (alphaInfo != kCGImageAlphaPremultipliedLast
+                    && alphaInfo != kCGImageAlphaLast
+                    && alphaInfo != kCGImageAlphaNoneSkipLast)
+                {
+                    shouldRedrawUsingCoreGraphics = YES;
+                }
+                else
+                {
+                    format = GL_RGBA;
+                }
+            }
+        }
     }
-    _texture = _textureInfo.name;
-    _size = KTVVPSizeMake(_textureInfo.width, _textureInfo.height);
+    if (shouldRedrawUsingCoreGraphics)
+    {
+        imageData = (GLubyte *)calloc(1, _size.width * _size.height * 4);
+        CGColorSpaceRef genericRGBColorspace = CGColorSpaceCreateDeviceRGB();
+        CGContextRef imageContext = CGBitmapContextCreate(imageData,
+                                                          _size.width,
+                                                          _size.height,
+                                                          8,
+                                                          _size.width * 4,
+                                                          genericRGBColorspace,
+                                                          kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedFirst);
+        CGContextDrawImage(imageContext,
+                           CGRectMake(0.0, 0.0, _size.width, _size.height),
+                           cgImage);
+        CGContextRelease(imageContext);
+        CGColorSpaceRelease(genericRGBColorspace);
+    }
+    else
+    {
+        dataFromImageDataProvider = CGDataProviderCopyData(CGImageGetDataProvider(cgImage));
+        imageData = (GLubyte *)CFDataGetBytePtr(dataFromImageDataProvider);
+    }
+    
+    glGenTextures(1, &_texture);
+    glBindTexture(GL_TEXTURE_2D, _texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexImage2D(GL_TEXTURE_2D,
+                 0,
+                 GL_RGBA,
+                 _size.width,
+                 _size.height,
+                 0,
+                 format,
+                 GL_UNSIGNED_BYTE,
+                 imageData);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    
+    if (shouldRedrawUsingCoreGraphics)
+    {
+        free(imageData);
+    }
+    else
+    {
+        if (dataFromImageDataProvider)
+        {
+            CFRelease(dataFromImageDataProvider);
+        }
+    }
 }
 
 - (void)destory
 {
-    if (_textureInfo)
-    {
-        _textureInfo = nil;
-    }
     if (_texture)
     {
         glDeleteTextures(1, &_texture);
