af961cdef7f635489670be49186ab8f21f065f3c
Single <libobjc@gmail.com>
2018-05-29 16:36:43 +0800

    public header

--- KTVVideoProcess/KTVVideoProcess.h
+++ KTVVideoProcess/KTVVideoProcess.h
@@ -6,14 +6,72 @@
 //  Copyright © 2018年 Single. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <UIKit/UIKit.h"
 
-//! Project version number for KTVVideoProcess.
 FOUNDATION_EXPORT double KTVVideoProcessVersionNumber;
-
-//! Project version string for KTVVideoProcess.
 FOUNDATION_EXPORT const unsigned char KTVVideoProcessVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <KTVVideoProcess/PublicHeader.h>
+#pragma mark - Common
 
+#import "KTVVPContext.h"
+#import "KTVVPDefines.h"
 
+#pragma mark - Source
+
+#import "KTVVPSource.h"
+#import "KTVVPCaptureSession.h"
+#import "KTVVPAVPlayerItemVideoOutput.h"
+
+#pragma mark - Pipeline
+
+#import "KTVVPPipeline.h"
+#import "KTVVPSerialPipeline.h"
+#import "KTVVPConcurrentPipeline.h"
+
+#pragma mark - Basic Filter
+
+#import "KTVVPFilter.h"
+#import "KTVVPTransformFilter.h"
+#import "KTVVPPassthroughFilter.h"
+
+#pragma mark - Output
+
+#import "KTVVPFrameView.h"
+#import "KTVVPFrameWriter.h"
+
+#pragma mark - Frame
+
+#import "KTVVPFrameInput.h"
+#import "KTVVPFrame.h"
+#import "KTVVPFramePool.h"
+#import "KTVVPFrameLayout.h"
+#import "KTVVPFrameUploader.h"
+#import "KTVVPGLTextureFrame.h"
+#import "KTVVPGLDrawableFrame.h"
+#import "KTVVPCVPixelBufferFrame.h"
+#import "KTVVPCMSmapleBufferFrame.h"
+
+#pragma mark - Sample
+
+#import "KTVVPSampleInput.h"
+#import "KTVVPSample.h"
+
+#pragma mark - OpenGL
+
+#import "KTVVPGLDefines.h"
+#import "KTVVPGLModel.h"
+#import "KTVVPGLPlaneModel.h"
+#import "KTVVPGLProgram.h"
+#import "KTVVPGLStandardProgram.h"
+#import "KTVVPGLImageTexture.h"
+#import "KTVVPGLUtils.h"
+
+#pragma mark - Export
+
+#import "KTVVPExportSession.h"
+#import "KTVVPExportReader.h"
+#import "KTVVPExportWriter.h"
+
+#pragma mark - Extension
+
+#import "KTVVPBlackAndWhiteFilter.h"
