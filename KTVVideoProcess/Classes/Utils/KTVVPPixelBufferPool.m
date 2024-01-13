0e6382b3276f5913c5bd00f6f0c5eef7ef53c32c
Gary Zhao <libobjc@gmail.com>
2024-01-13 19:33:21 +0800

    Revert "Update Utils."
    This reverts commit 33e5b10cb15933ef16aa7a74d84a2991e8bdb56a.

--- KTVVideoProcess/Classes/Utils/KTVVPPixelBufferPool.m
+++ KTVVideoProcess/Classes/Utils/KTVVPPixelBufferPool.m
@@ -19,7 +19,8 @@
 
 - (void)dealloc
 {
-    if (_pixelBufferPool) {
+    if (_pixelBufferPool)
+    {
         CVPixelBufferPoolRelease(_pixelBufferPool);
         _pixelBufferPool = NULL;
     }
@@ -28,8 +29,9 @@
 - (CVPixelBufferRef)copyPixelBuffer:(CVPixelBufferRef)pixelBuffer
 {
     CVReturn error;
-    if (!_pixelBufferPool) {
-        NSMutableDictionary *attributes = [NSMutableDictionary dictionary];
+    if (!_pixelBufferPool)
+    {
+        NSMutableDictionary * attributes = [NSMutableDictionary dictionary];
         [attributes setObject:@(CVPixelBufferGetPixelFormatType(pixelBuffer))
                        forKey:(id)kCVPixelBufferPixelFormatTypeKey];
         [attributes setObject:@(CVPixelBufferGetWidth(pixelBuffer))
@@ -41,21 +43,23 @@
         [attributes setObject:@{}
                        forKey:(id)kCVPixelBufferIOSurfacePropertiesKey];
         error = CVPixelBufferPoolCreate(kCFAllocatorDefault, NULL, (__bridge CFDictionaryRef)attributes, &_pixelBufferPool);
-        if (error != kCVReturnSuccess) {
+        if (error != kCVReturnSuccess)
+        {
             KTVVPLog(@"create CVPixelBufferPool failed");
         }
     }
     CVPixelBufferRef ret = NULL;
     error = CVPixelBufferPoolCreatePixelBuffer(NULL, _pixelBufferPool, &ret);
-    if(error != kCVReturnSuccess) {
+    if(error != kCVReturnSuccess)
+    {
         KTVVPLog(@"create CVPixelBuffer failed");
     }
     CVPixelBufferLockBaseAddress(ret, 0);
     CVPixelBufferLockBaseAddress(pixelBuffer, 0);
     size_t dataSize = CVPixelBufferGetDataSize(ret);
     size_t sourceDataSize = CVPixelBufferGetDataSize(pixelBuffer);
-    void *dataPointer = CVPixelBufferGetBaseAddress(ret);
-    void *sourceDataPointer = CVPixelBufferGetBaseAddress(pixelBuffer);
+    void * dataPointer = CVPixelBufferGetBaseAddress(ret);
+    void * sourceDataPointer = CVPixelBufferGetBaseAddress(pixelBuffer);
     memcpy(dataPointer, sourceDataPointer, MIN(dataSize, sourceDataSize));
     CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
     CVPixelBufferUnlockBaseAddress(ret, 0);
--- KTVVideoProcess/Classes/Utils/KTVVPTimeComponents.m
+++ KTVVideoProcess/Classes/Utils/KTVVPTimeComponents.m
@@ -19,7 +19,8 @@
 
 - (instancetype)init
 {
-    if (self = [super init]) {
+    if (self = [super init])
+    {
         _timeStamp = kCMTimeZero;
         _previousTimeStamp = kCMTimeZero;
         _firstTimeStamp = kCMTimeInvalid;
@@ -32,21 +33,24 @@
 
 - (void)putDroppedTimeStamp:(CMTime)timeStamp
 {
-    if (CMTIME_IS_INVALID(_firstDroppedTimeStamp)) {
+    if (CMTIME_IS_INVALID(_firstDroppedTimeStamp))
+    {
         _firstDroppedTimeStamp = timeStamp;
     }
 }
 
 - (void)putCurrentTimeStamp:(CMTime)timeStamp
 {
-    if (CMTIME_IS_VALID(_firstDroppedTimeStamp)) {
+    if (CMTIME_IS_VALID(_firstDroppedTimeStamp))
+    {
         CMTime currentDeltaInterval = CMTimeSubtract(timeStamp, _firstDroppedTimeStamp);
         _deltaInterval = CMTimeAdd(_deltaInterval, currentDeltaInterval);
         _firstDroppedTimeStamp = kCMTimeInvalid;
     }
     _previousTimeStamp = _timeStamp;
     _timeStamp = CMTimeSubtract(timeStamp, _deltaInterval);
-    if (CMTIME_IS_INVALID(_firstTimeStamp)) {
+    if (CMTIME_IS_INVALID(_firstTimeStamp))
+    {
         _firstTimeStamp = _timeStamp;
     }
     _duration = CMTimeSubtract(_timeStamp, _firstTimeStamp);
